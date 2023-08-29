4ac87668671a668996e50873cf19ddf6831cbf71
Allan Oliveira <allanolme@gmail.com>
2023-08-29 18:07:25 -0300

    Create round().py

---
+++ round().py
@@ -0,0 +1,8 @@
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles
+kilometers_to_miles = kilometers
+
+print(miles, "milhas é", round(miles_to_kilometers, 1), "quilômetros")
+print(kilometers, "quilômetros é", round(kilometers_to_miles, 2), "milhas")
