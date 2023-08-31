7b24ec2c66469009291624531d55bd9e0e151c6a
Allan Oliveira <allanolme@gmail.com>
2023-08-31 13:16:15 -0300

    Create condition.py

---
+++ condition.py
@@ -0,0 +1,20 @@
+print(2 == 2)
+print(2 == 3)
+print(2 != 2)
+print(2 != 3)
+
+var1 = True
+if var1:
+    print("\nYes,")
+    print(True)
+else:
+    print("\nNo,")
+    print(False)
+
+num1 = 20
+num2 = 10
+
+if num1 < num2:
+    print("\n", num1, "é menor que", num2)
+else:
+    print("\n", num1, "é maior que", num2)
