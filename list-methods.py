4e4dc278e9c817fb795a819bab72ab4d5a0a695d
Allan Oliveira <allanolme@gmail.com>
2023-09-06 18:02:21 -0300

    Create list-methods.py

---
+++ list-methods.py
@@ -0,0 +1,11 @@
+numbers = [111, 7, 2, 1]
+print(len(numbers))
+print(numbers)
+
+numbers.append (4)
+print(len(numbers))
+print(numbers)
+
+numbers.insert (1, 222)
+print(len (numbers))
+print(numbers)
