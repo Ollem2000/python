5e78b8229761428e20231efded25746f98fa7063
Allan Oliveira <allanolme@gmail.com>
2023-09-07 22:34:11 -0300

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+dictionary = {"gato": "chat", "cachorro": "chien", "cavalo": "cheval"}
+phone_numbers = {'chefe': 5551234567, 'Suzy': 22657854310}
+empty_dictionary = {}
+ 
+print(dictionary)
+print(phone_numbers)
+print(empty_dictionary)
