96f7c14992f3d525be0b65c3649fdf870303f208
Allan Oliveira <allanolme@gmail.com>
2023-09-07 22:34:14 -0300

    Create dictionary-check.py

---
+++ dictionary-check.py
@@ -0,0 +1,8 @@
+dictionary = {"gato": "chat", "cachorro": "chien", "cavalo": "cheval"}
+words = ['gato', 'lion', 'cavalo']
+ 
+for word in words:
+    if word in dictionary:
+        print(word, "->", dictionary[word])
+    else:
+        print(word, "não está no dicionário")
